3e7ba25fdaaa0225c38844e9189135f4a4839b29
Mdigennaro <michele.digennaro719@gmail.com>
2021-11-24 13:07:49 +0100

    aggiunto logo in header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <div id="app">
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue"
 
 export default {
   name: 'App',
   components: {
+    Header
   }
+
 }
 </script>
 
 <style lang="scss">
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app{
+  width: 100%;
+  min-width: 1180px;
+}
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <header>
+
+    <div class="container">
+      <div class="logo">
+        <img src="../assets/img/dc-logo.png" alt="">
+      </div>
+      <div class="menu">
+        <nav>
+          <ul>
+            <li><a href=""></a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style lang="scss">
+
+header{
+  background-color: aqua;
+
+  .container{
+    width: 70%;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 auto;
+
+    .logo img{
+      width: 65px;
+    
+    }
+
+  }
+
+}
+
+</style>
