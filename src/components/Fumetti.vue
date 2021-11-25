8411f6a288595c4e33e80279244743c3665604bb
Mdigennaro <michele.digennaro719@gmail.com>
2021-11-25 17:26:03 +0100

    ciclo for per i fumetti

---
+++ src/components/Fumetti.vue
@@ -0,0 +1,53 @@
+<template>
+
+<div class="fumetto-singolo">
+  <div class="fumetto-cover">
+    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+  </div>
+  
+  <h4>Action Comics</h4>
+</div>
+  
+</template>
+
+<script>
+export default {
+  nome: "Fumetti",
+  promps:{
+    imgFumetto: String,
+    nomeFumetto: String
+
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../assets/style/vars.scss";
+@import "../assets/style/mixins.scss";
+@import "../assets/style/utilities.scss";
+
+.fumetto-singolo{
+  flex-basis: calc(100% / 6);
+  height: 225px;
+  padding: 10px;
+
+  .fumetto-cover{
+    height: 150px;
+    overflow: hidden;
+    margin-bottom: 15px;
+    
+    img{
+      width: 100%;
+    }
+  }
+
+  h4{
+    color: white;
+    font-size: 11px;
+    font-weight: lighter;
+    text-transform: uppercase;
+  }
+
+}
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -10,7 +10,18 @@
 
     <section class="main-center">
 
-      <div class="container-dc"></div>
+      <div class="container-dc">
+        <div class="container">
+          <h3>Current series</h3>
+            <div class="box-fumetti">
+              <Fumetto v-for="(fumettoSingolo, index) in listaFumetti" :key="index"
+              :imgFumetto="" 
+              :nomeFumetto="" 
+              />
+
+            </div>
+        </div>
+      </div>
 
     </section>
 
@@ -31,14 +42,100 @@
 </template>
 
 <script>
+import Fumetto from './Fumetti.vue';
+
 export default {
-  name:"Main"
+  name:"Main",
+
+  components:{
+    Fumetto,
+  },
+
+  data(){
+    return{
+      listaFumetti:[
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+          "price": "$19.99",
+          "series": "Action Comics",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+          "price": "$3.99",
+          "series": "American Vampire 1976",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+          "price": "$16.99",
+          "series": "Aquaman",
+          "type": "graphic novel"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+          "price": "$2.99",
+          "series": "Batgirl",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+          "price": "$3.99",
+          "series": "Batman",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+          "price": "$2.99",
+          "series": "Batman Beyond",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+          "price": "$3.99",
+          "series": "Batman/Superman",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg? itok=azz5sfGk",
+          "price": "$4.99",
+          "series": "Batman/Superman Annual",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+          "price": "$5.99",
+          "series": "Batman: The Joker War Zone",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+          "price": "$6.99",
+          "series": "Batman: Three Jokers",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+          "price": "$4.99",
+          "series": "Batman: White Knight Presents: Harley Quinn",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+          "price": "$16.99",
+          "series": "Catwoman",
+          "type": "graphic novel"
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 @import "../assets/style/vars.scss";
 @import "../assets/style/mixins.scss";
+@import "../assets/style/utilities.scss";
 
 main{
   .main-top{
@@ -67,6 +164,33 @@ main{
       width: 100%;
       height: 62%;
       background-color: #1c1c1c;
+      display: flex;
+      flex-direction: column;
+
+      .container{
+        flex-direction: column;
+
+        h3{
+          align-self: flex-start;
+          color: white;
+          font-weight: bold;
+          text-transform: uppercase;
+          background-color: $primary-color;
+          padding: 10px;
+          margin-top: -20px;
+          font-size: 20px;
+          margin-bottom: 30px;
+        }
+
+        .box-fumetti{
+          @include allCenter();
+          justify-content: space-between;
+          flex-wrap: wrap;
+          
+        }
+
+      }
+
     }
 
   }
